cffc00f7e716e276c4ebc274ad0b3499d7b45c22
LuizFelipe2023 <142544096+LuizFelipe2023@users.noreply.github.com>
2024-02-09 14:40:34 -0400

    Update login.php

--- hash/login.php
+++ hash/login.php
@@ -23,18 +23,24 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
             echo "Usuário não existe ou não foi encontrado";
             exit;
         }
-        $token = generateJWT($email);
-        $stmt2 = $conn->prepare('UPDATE users SET token = ? WHERE email = ?');
-        $stmt2->bindParam(1, $token, PDO::PARAM_STR);
-        $stmt2->bindParam(2, $email, PDO::PARAM_STR);
-        $stmt2->execute();
 
-        $_SESSION['token'] = $token;
-        $_SESSION['email'] = $email;
+        // Verify password
+        if (password_verify($senha, $user['senha'])) {
+            $token = generateJWT($email);
+            $stmt2 = $conn->prepare('UPDATE users SET token = ? WHERE email = ?');
+            $stmt2->bindParam(1, $token, PDO::PARAM_STR);
+            $stmt2->bindParam(2, $email, PDO::PARAM_STR);
+            $stmt2->execute();
 
-        echo "Login bem-sucedido!";
-        header('Location: home.php');
-        exit();
+            $_SESSION['token'] = $token;
+            $_SESSION['email'] = $email;
+
+            echo "Login bem-sucedido!";
+            header('Location: home.php');
+            exit();
+        } else {
+            echo "Senha incorreta!";
+        }
     }
 }
 
@@ -49,34 +55,3 @@ function generateJWT($email)
     return $token;
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <!-- Adicionando Bootstrap -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="/./public/style.css">
-</head>
-<body>
-    <div class="container pt-5">
-        <div class="login-form">
-            <h2 class="text-center">Login</h2>
-            <form action="login.php" method="post">
-                <div class="form-group">
-                    <label for="email">Email:</label>
-                    <input type="email" class="form-control" name="email" id="email" required>
-                </div>
-                <div class="form-group">
-                    <label for="senha">Senha:</label>
-                    <input type="password" class="form-control" name="senha" id="senha" required>
-                </div>
-                <button type="submit" class="btn btn-primary btn-block">Enviar</button>
-            </form>
-        
-        </div>
-        <a href="cadastrar.php" class="btn btn-primary text-center mt-4">Não tem Conta? Clique aqui para fazer uma</a>
-    </div>
-</body>
-</html>
